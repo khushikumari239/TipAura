fd8de4d9b856784c5a019b81355cef474f07c6b3
Khushiii👩‍💻 <145993686+khushikumari239@users.noreply.github.com>
2025-11-07 18:59:54 +0530

    add new changes

--- TipAura/app/page.tsx
+++ TipAura/app/page.tsx
@@ -2,15 +2,42 @@
 
 import { useEffect } from "react";
 import Image from "next/image";
-import { Wallet } from "@coinbase/onchainkit/wallet";
+// import { ConnectWallet, Wallet } from "@coinbase/onchainkit/wallet";
 import { useMiniKit } from "@coinbase/onchainkit/minikit";
 import { createBaseAccountSDK, base } from "@base-org/account";
 import '@coinbase/onchainkit/styles.css';
 
-import styles from "./page.module.css";
+import { Avatar, Name } from '@coinbase/onchainkit/identity';
 
-export default function Home() {
+import type { LifecycleStatus } from '@coinbase/onchainkit/transaction';
+import { Wallet, ConnectWallet } from '@coinbase/onchainkit/wallet';
+import { useAccount } from 'wagmi';
+import { calls } from '@/calls'
+
+// @noErrors: 1109 - Cannot find name 'contracts'
+// import { Transaction } from "@coinbase/onchainkit/transaction"
+import { 
+  Transaction, 
+  TransactionButton,
+  TransactionSponsor,
+  TransactionStatus,
+  TransactionStatusAction,
+  TransactionStatusLabel,
+} from '@coinbase/onchainkit/transaction'; 
+
+// const calls = [...];
+
+{/* <Transaction calls={calls} /> */}
+
+import styles from "./page.module.css";
+import { OnchainKitProvider } from "@coinbase/onchainkit";
+import { baseSepolia } from "viem/chains";
+
+export default function Home({ }: { children?: React.ReactNode }) {
   const { context } = useMiniKit();
+
+const BASE_SEPOLIA_CHAIN_ID = 84532;
+
 
   // Initialize SDK inside component
   const sdk = createBaseAccountSDK({
@@ -20,13 +47,63 @@ export default function Home() {
   });
 
   const provider = sdk.getProvider(); // use for transactions
+  
+  return address ? (
+    <Transaction
+      chainId={BASE_SEPOLIA_CHAIN_ID}
+      calls={calls}
+      onStatus={handleOnStatus}
+    >
+      <TransactionButton />
+      <TransactionSponsor />
+      <TransactionStatus>
+        <TransactionStatusLabel />
+        <TransactionStatusAction />
+      </TransactionStatus>
+    </Transaction>
+  ) : (
+    <Wallet>
+      <ConnectWallet>
+        <Avatar className='h-6 w-6' />
+       {/* </Name/> */}
+       
+      </ConnectWallet>
+      </Wallet>
+  );
+};
 
-  // Example useEffect (uncomment if needed)
-  // useEffect(() => {
-  //   if (!isMiniAppReady) {
-  //     setMiniAppReady();
-  //   }
-  // }, [setMiniAppReady, isMiniAppReady]);
+
+
+
+  
+  <OnchainKitProvider
+    apiKey={process.env.NEXT_PUBLIC_CDP_API_KEY}
+    chain={baseSepolia}
+    config={{ paymaster: process.env.NEXT_PUBLIC_PAYMASTER_AND_BUNDLER_ENDPOINT }}
+  >
+    <div className={styles.container}>
+      <header className={styles.headerWrapper}>
+        <Wallet />
+      </header>
+
+      <div className={styles.content}>
+        <Image
+          priority
+          src="/Tips.png"
+          alt="TipsLogo"
+          width={200}
+          height={200} />
+        <h1 className={styles.title}>TipAura</h1>
+
+        <h2>Welcome {context?.user?.displayName}!</h2>
+        <p>FID: {context?.user?.fid}</p>
+      </div>
+    </div>
+  </OnchainKitProvider>
+  
+  
+
+
 
   return (
     <div className={styles.container}>
@@ -47,25 +124,6 @@ export default function Home() {
         <h2>Welcome {context?.user?.displayName}!</h2>
         <p>FID: {context?.user?.fid}</p>
 
-
-
-        {/* <h2 className={styles.componentsTitle}>Explore Components</h2> */}
-
-        {/* <ul className={styles.components}>
-          {[
-            { name: "Transaction", url: "https://docs.base.org/onchainkit/transaction/transaction" },
-            { name: "Swap", url: "https://docs.base.org/onchainkit/swap/swap" },
-            { name: "Checkout", url: "https://docs.base.org/onchainkit/checkout/checkout" },
-            { name: "Wallet", url: "https://docs.base.org/onchainkit/wallet/wallet" },
-            { name: "Identity", url: "https://docs.base.org/onchainkit/identity/identity" },
-          ].map((component) => (
-            <li key={component.name}>
-              <a target="_blank" rel="noreferrer" href={component.url}>
-                {component.name}
-              </a>
-            </li>
-          ))}
-        </ul> */}
       </div>
     </div>
   );
